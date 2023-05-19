4c0c74f72b4cea437dacc87919a4dcd1090eb9b2
Markpm-code <marlonseglos@yahoo.com>
2023-05-19 19:45:00 +0000

    Added allowed host link and edit text

--- pfs_affordables/settings.py
+++ pfs_affordables/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['8000-markpmcode-palalatfashi-94c5xg3ef5j.ws-eu97.gitpod.io']
 
 
 # Application definition
