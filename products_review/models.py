4943b152c45f4c19b46db88685c27fd83a32520a
Markpm-code <marlonseglos@yahoo.com>
2023-05-30 10:23:08 +0000

    Added product review app

---
+++ products_review/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+from products.models import Product
+
+# Create your models here.
+
+# Product Review
+
+class ProductReview(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    review_text = models.TextField()
