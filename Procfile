6b2ec7366b8eb7c98d9b36bb8bcfc766597cd376
Markpm-code <marlonseglos@yahoo.com>
2023-06-09 23:05:33 +0000

    commit initial deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn pfs_affordables.wsgi:application
--- pfs_affordables/settings.py
+++ pfs_affordables/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 if os.path.isfile('env.py'):
     import env
 
@@ -28,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-markpmcode-palalatfashi-n90cu06q89y.ws-eu99.gitpod.io' , 'localhost']
+ALLOWED_HOSTS = ['palalat-fashion.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -112,6 +113,11 @@ WSGI_APPLICATION = 'pfs_affordables.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+   'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    }
+else:
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
