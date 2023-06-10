f84a33a66df5218d92ebd3aca3356789d9cfd70f
Markpm-code <marlonseglos@yahoo.com>
2023-06-10 22:16:02 +0000

    Test multi host

--- pfs_affordables/settings.py
+++ pfs_affordables/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-markpmcode-palalatfashi-n90cu06q89y.ws-eu99.gitpod.io', 'localhost']
+ALLOWED_HOSTS = ['8000-markpmcode-palalatfashi-n90cu06q89y.ws-eu99.gitpod.io', 'palalat-fashion.herokuapp.com' 'localhost']
 
 
 # Application definition
