427d7cb42108d5a6696b447139da54b0148b65f5
Markpm-code <marlonseglos@yahoo.com>
2023-06-12 02:05:56 +0000

    Added footer template, initial functionality and set up newsletter

--- pfs_affordables/settings.py
+++ pfs_affordables/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['palalat-fashion.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['8000-markpmcode-palalatfashi-jih8ncy9xb8.ws-eu99.gitpod.io', 'localhost']
 
 
 # Application definition
