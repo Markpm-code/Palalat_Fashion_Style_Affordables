d992c7403ce064604e8c4bc5e57e4cd452612298
Markpm-code <marlonseglos@yahoo.com>
2023-07-27 15:58:37 +0000

    Added initial set up of custom 404 page

--- pfs_affordables/urls.py
+++ pfs_affordables/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +26,5 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('cart/', include('cart.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'pfs_affordables.views.handler404'
